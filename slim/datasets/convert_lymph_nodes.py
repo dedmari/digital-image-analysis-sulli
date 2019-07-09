01fe3b34f44ac8151c1c8cc041d482fa3b0a274e
muneer7589 <muneer7589@gmail.com>
2019-07-09 22:21:16 +0200

    removing lymp_node_ from tf_record filenames for training and validation

--- slim/datasets/convert_lymph_nodes.py
+++ slim/datasets/convert_lymph_nodes.py
@@ -112,7 +112,7 @@ def _get_filenames_and_classes(dataset_dir):
 
 
 def _get_dataset_filename(dataset_dir, split_name, shard_id):
-  output_filename = 'lymph_nodes_%s_%05d-of-%05d.tfrecord' % (
+  output_filename = '%s_%05d-of-%05d' % (
       split_name, shard_id, _NUM_SHARDS)
   return os.path.join(dataset_dir, output_filename)
 
